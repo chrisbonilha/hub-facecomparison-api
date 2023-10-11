b4f16a3394458fba8f0dac3afea6acdcaa636c7c
Chris Bonilha <christiane.bonilha@vmetrix.com>
2023-10-11 17:00:28 -0300

    fix: properties before all tests

--- src/test/java/com/br/hub/facecomparison/FacecomprisonApplicationTests.java
+++ src/test/java/com/br/hub/facecomparison/FacecomprisonApplicationTests.java
@@ -1,5 +1,6 @@
 package com.br.hub.facecomparison;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,6 +10,13 @@ import com.br.hub.facecomparison.config.FacecomprisonApplication;
 @SpringBootTest(classes = FacecomprisonApplication.class)
 @ActiveProfiles(value = "test")
 class FacecomprisonApplicationTests {
+
+	@BeforeAll
+	static void setupProperties() {
+		System.setProperty("aws.region", "us-east-1");
+		System.setProperty("aws.accessKeyId", "localstack");
+		System.setProperty("aws.secretAccessKey", "localstack");
+	}
 
 	@Test
 	void contextLoads() {
