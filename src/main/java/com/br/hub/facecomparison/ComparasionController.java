5c9119b7c6e19a1fcd9a03941c4ee9ca1f724979
Chris Bonilha <christiane.bonilha@vmetrix.com>
2023-10-11 18:35:22 -0300

    enable cross

--- src/main/java/com/br/hub/facecomparison/ComparasionController.java
+++ src/main/java/com/br/hub/facecomparison/ComparasionController.java
@@ -2,13 +2,14 @@ package com.br.hub.facecomparison;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(path = "/facecomparison", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ComparasionController {
